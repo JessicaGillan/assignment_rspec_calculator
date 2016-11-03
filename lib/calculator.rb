c5ef7c1b8717bf4e4c18c87f180ce847ea8a9547
Jessica G <gillan.jessica@gmail.com>
2016-11-03 10:17:01 -0600

    tests for add and subtract

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -7,32 +7,32 @@ class Calculator
     output(a+b)
   end
   def subtract(a,b)
-    output(a-b)
+    # output(a-b)
   end
   def multiply(a,b)
-    output(a*b)
+    # output(a*b)
   end
   def divide(a,b)
-    raise ArgumentError if b == 0
-    result = a.to_f/b.to_f
-    result.to_i == result ? output(result.to_i) : output(result)
+    # raise ArgumentError if b == 0
+    # result = a.to_f/b.to_f
+    # result.to_i == result ? output(result.to_i) : output(result)
   end
   def pow(a,b)
-    output((a**b).to_f)
+    # output((a**b).to_f)
   end
   def sqrt(a)
-    raise ArgumentError if a < 0
-    root = a**0.5
-    root == root.to_i ? output(root.to_i) : output(root.round(2))
+    # raise ArgumentError if a < 0
+    # root = a**0.5
+    # root == root.to_i ? output(root.to_i) : output(root.round(2))
   end
   # returns the item in memory and wipes memory
   def memory
-    mem = @memory
-    @memory = nil
-    return mem
+    # mem = @memory
+    # @memory = nil
+    # return mem
   end
   def memory=(mem)
-    @memory = mem
+    # @memory = mem
   end
   def output(result)
     @stringify ? result.to_s : result
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,42 @@
-# Your code here
+require 'calculator'
+
+describe Calculator do
+
+	let(:calculator) { Calculator.new }
+	
+	describe '#add' do
+		it "adds two positive integer numbers" do
+			expect(calculator.add(1,1)).to eq(2)
+		end
+
+		it "adds two positive float numbers" do
+			expect(calculator.add(1.0,1.1)).to eq(2.1)
+		end
+
+		it "adds two negative integers" do
+			expect(calculator.add(-1,-1)).to eq(-2)
+		end
+
+		it "adds two negative flaot numbers" do
+			expect(calculator.add(-1.0,-1.1)).to eq(-2.1)
+		end
+	end
+
+	describe '#subtract' do
+		it "subtract two positive integer numbers" do
+			expect(calculator.subtract(1,1)).to eq(0)
+		end
+
+		it "subtract two positive float numbers" do
+			expect(calculator.subtract(1.0,1.1)).to eq(-0.1)
+		end
+
+		it "subtract two negative integers" do
+			expect(calculator.subtract(-1,-1)).to eq(0)
+		end
+
+		it "subtract two negative flaot numbers" do
+			expect(calculator.subtract(-1.0,-1.1)).to eq(0.1)
+		end
+	end
+end
